9ebd0d53626fd4b1482170c7c173e4aff6e349f7
Roshith P <roshith844@gmail.com>
2023-01-09 10:45:34 +0530

    add font awsome icon

--- my-app/src/app/components/task-item/task-item.component.ts
+++ my-app/src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,6 @@
 import { Component,OnInit, Input} from '@angular/core';
 import {Task} from '../../Tasks'
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
@@ -7,6 +8,7 @@ import {Task} from '../../Tasks'
 })
 export class TaskItemComponent implements OnInit {
   @Input() task: any;
+ faTimes = faTimes
   constructor(){}
   ngOnInit(): void {
       
